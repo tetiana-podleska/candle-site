4bbf19ff0c7c4e95c62ccdc38aa840e52d08d30f
Tetiana Podleska <podleskatetiana@gmail.com>
2023-08-10 15:12:08 +0300

    team section / media finished feedback section / desctop markup finished

--- app/js/main.js
+++ app/js/main.js
@@ -4,3 +4,11 @@ const menuMobile = document.querySelector(".header__menu-list");
 menuBtn.addEventListener("click", () => {
   menuMobile.classList.toggle("menu--open");
 });
+
+const swiperOne = new Swiper(".feedback__slider", {
+  loop: true,
+
+  pagination: {
+    el: ".swiper-pagination",
+  },
+});
